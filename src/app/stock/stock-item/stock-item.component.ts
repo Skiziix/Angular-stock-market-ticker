c2a43555727069609671d6fc72530f0ce5bdef35
bashfulmonkey <rearick.seth@gmail.com>
2024-08-27 11:48:59 -0400

    Apply styles to a stock-item based upon price changed with NgStyle.

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Stock } from '../../model/stock';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-stock-item',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './stock-item.component.html',
   styleUrl: './stock-item.component.css'
 })
 export class StockItemComponent implements OnInit {
-  public stock!: Stock
+  public stock!: Stock;
+  public stockClasses!: Object;
 
   constructor () {}
 
   ngOnInit() {
     this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+    let diff = (this.stock.price / this.stock.previousPrice) - 1;
+    let largeChange = Math.abs(diff) > 0.01;
+    this.stockClasses = {
+        "positive": this.stock.isPositiveChange(),
+        "negative": !this.stock.isPositiveChange(),
+        "large-change": largeChange,
+        "small-change": !largeChange
+    };
   }
 
   toggleFavorite(event: Event) {
